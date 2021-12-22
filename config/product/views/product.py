4d00a42fbe7a198799a8956b566cac91d6c5ce6e
jayednahian <jayednahian@yahoo.com>
2021-12-22 17:05:00 +0600

    data_list-(complete pagination)

--- config/product/views/product.py
+++ config/product/views/product.py
@@ -2,7 +2,7 @@ from django.views import generic
 # from django.views.generic import ListView,DetailView,TemplateView
 from django.shortcuts import render
 from product.models import Variant,ProductVariant,Product,ProductVariantPrice
-
+from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
 
 class CreateProductView(generic.TemplateView):
     template_name = 'products/create.html'
@@ -28,12 +28,28 @@ class ListView(generic.ListView):
 
 def list_view(request):
     ProductVariantPrice_qs = ProductVariantPrice.objects.all()
-    product_qs =Product.objects.all()
+    product_qs =Product.objects.all().order_by('id')
     ProductVariant_qs = ProductVariant.objects.all()
+    total_data = product_qs.count()
+
+    pagination_post = Paginator(product_qs,2)
+    page_number = request.GET.get('page')
+    #page_obj = pagination_post.get_page(page_number)
+    try:
+        page_obj = pagination_post.get_page(page_number)
+    except PageNotAnInteger:
+        page_obj = pagination_post.get_page(1)
+    except EmptyPage:
+        page_obj = pagination_post.get_page(pagination_post.num_page)
+
+
+
 
     context={
+        'total_data':total_data,
+        'pagination_post':pagination_post.page_range ,
         'ProductVariantPrice_qs':ProductVariantPrice_qs,
-        'product_qs':product_qs,
+        'product_qs':page_obj,
         'ProductVariant_qs':ProductVariant_qs
 
     }
